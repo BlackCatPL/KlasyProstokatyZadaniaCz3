ad65de51bcd9771f85413d6edc74df4d00e9f467
TOMCAT-DES\ssj00 <tomasz.kocikowski84@gmail.com>
2022-01-13 20:56:16 +0100

    Add project files.

---
+++ Klasy cz3 - zadania/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Klasy_cz3_zadania;
+
+Console.WriteLine("Klasy cz.3 - zadania !");
+
+Console.Write("Podaj długość dla p1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Podaj szerokość dla p1: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n");
+
+Console.Write("Podaj długość dla p2: ");
+int c = int.Parse(Console.ReadLine());
+Console.Write("Podaj szerokość dla p2: ");
+int d = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n Pierwszy prostokąt ma następujące wyniki: ");
+Prostokat p1 = new Prostokat(a, b);
+p1.Prezentuj();
+Console.ReadKey();
+Console.WriteLine("\n Drugi prostokąt ma następujące wyniki: ");
+Prostokat p2 = new Prostokat(c, d);
+p2.Prezentuj();
+Console.ReadKey();
+
+Prostokat[] prostokaty = new Prostokat[2];
+prostokaty[0] = p1;
+prostokaty[1] = p2;
+
+Console.WriteLine("\n");
+
+
+foreach (var prostokat in prostokaty)
+{
+    prostokat.Prezentuj();
+}
+Console.WriteLine("\n");
+Console.WriteLine("Największa powierzchnia prostokąta: {0}", Prostokat.Najwieksze(prostokaty));
+Console.ReadKey();
+
+
+
+
+
+
+// 1. Uzupełnij wcześniej utworzoną klasę prostokąt o definicję tablicy obiektów dla prostokątów. 
+// W metodzie Main() wyświetl powierzchnie oraz obwód wszystkich prostokątów w tablicy 
+// używając (wewnątrz pętli foreach) metody publicznej Prezentuj(). 
+
+// 2. Uzupełnij program z poprzedniego zadania o definicję metody statycznej,
+// która podaje powierzchnię największego prostokąta. 
---
+++ Klasy cz3 - zadania/Prostokat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasy_cz3_zadania
+{
+    internal class Prostokat
+    {
+        int dlugosc;
+        int szerokosc;
+        private int p;
+        private int o;
+
+        public Prostokat(int d, int s)
+        {
+            dlugosc = d;
+            szerokosc = s;
+        }
+
+        private double Powierzchnia()
+        {
+            p = dlugosc * szerokosc;
+            return p;
+        }
+        private double Obwod()
+        {
+            o = 2 * (dlugosc + szerokosc);
+            return o;
+        }
+
+        public void Prezentuj()
+        {
+            Powierzchnia();
+            Obwod();
+            Console.WriteLine("Powierzchnia: {0}", p);
+            Console.WriteLine("Obwód: {0}", o);
+        }
+
+        public static double Najwieksze(Prostokat[] tab)
+        { 
+            double najwieksze = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {                
+                najwieksze = Math.Max(najwieksze, tab[i].Powierzchnia());                
+            }
+            return najwieksze;
+        }
+    }
+    // 2. Uzupełnij program z poprzedniego zadania o definicję metody statycznej,
+    // która podaje powierzchnię największego prostokąta.
+}
